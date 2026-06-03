1e687195c37fa23de01e140fb9d950b89b1fae72
Mohamed Kalith <kalith1218@gmail.com>
2026-06-03 10:59:42 +0530

    deploy

--- Backend/backend/settings.py
+++ Backend/backend/settings.py
@@ -8,7 +8,11 @@ SECRET_KEY = 'django-insecure-change-this-secret-key'
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    ".onrender.com"
+]
 
 
 INSTALLED_APPS = [
@@ -137,7 +141,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "https://project-jobfind.vercel.app",
+    "http://localhost:5173"
+]
 
 
 # DRF
