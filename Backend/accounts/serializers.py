de34296c2b8eaf1bc2faf86d6b7969155154695a
Mohamed Kalith <kalith1218@gmail.com>
2026-06-07 15:47:24 +0530

    Fix profile creation

--- Backend/accounts/serializers.py
+++ Backend/accounts/serializers.py
@@ -23,37 +23,37 @@ class RegisterSerializer(serializers.ModelSerializer):
             }
         }
 
-    def create(self, validated_data):
-
-        role = validated_data.pop('role')
-
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password']
-        )
-
-        user.profile.role = role
-        user.profile.save()
-
-        return user
-
     # def create(self, validated_data):
 
     #     role = validated_data.pop('role')
 
     #     user = User.objects.create_user(
-    #        username=validated_data['username'],
-    #        email=validated_data['email'],
-    #        password=validated_data['password']
+    #         username=validated_data['username'],
+    #         email=validated_data['email'],
+    #         password=validated_data['password']
     #     )
 
-    #     Profile.objects.create(
-    #        user=user,
-    #        role=role
-    #     )
+    #     user.profile.role = role
+    #     user.profile.save()
 
     #     return user
+
+    def create(self, validated_data):
+
+        role = validated_data.pop('role')
+
+        user = User.objects.create_user(
+           username=validated_data['username'],
+           email=validated_data['email'],
+           password=validated_data['password']
+        )
+
+        Profile.objects.create(
+           user=user,
+           role=role
+        )
+
+        return user
 
 
 class ProfileSerializer(serializers.ModelSerializer):
